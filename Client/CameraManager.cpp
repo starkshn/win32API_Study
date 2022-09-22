33bf8ada2a6ef666daa7b72d342f34b1e3d1f3d9
starkshn <starkshn97@gmail.com>
2022-09-22 11:59:57 +0900

    다시

--- Client/CameraManager.cpp
+++ Client/CameraManager.cpp
@@ -32,14 +32,15 @@ void CameraManager::update()
 	}
 
 	// camera이동 테스트
-	//if (KEY_HOLD(KEY::UP))
-	//	_lookAtPos._y -= 500.f * DeltaTime_F;
-	//if (KEY_HOLD(KEY::DOWN))
-	//	_lookAtPos._y += 500.f * DeltaTime_F;
-	//if (KEY_HOLD(KEY::LEFT))
-	//	_lookAtPos._x -= 500.f * DeltaTime_F;
-	//if (KEY_HOLD(KEY::RIGHT))
-	//	_lookAtPos._x += 500.f * DeltaTime_F;
+	if (KEY_HOLD(KEY::UP))
+		_lookAtPos._y -= 500.f * DeltaTime_F;
+	if (KEY_HOLD(KEY::DOWN))
+		_lookAtPos._y += 500.f * DeltaTime_F;
+	if (KEY_HOLD(KEY::LEFT))
+		_lookAtPos._x -= 500.f * DeltaTime_F;
+	if (KEY_HOLD(KEY::RIGHT))
+		_lookAtPos._x += 500.f * DeltaTime_F;
+
 	
 	// 화면 중앙좌표와 LookAt 좌표간의 차이값계산
 	CalDiff();
@@ -58,21 +59,7 @@ void CameraManager::CalDiff()
 	else
 	{
 		Vector2 lookDir = _lookAtPos - _prevLookPos;
-
-		// 등속도 운동
-		// _corLookPos = _prevLookPos + lookDir.Normalize() * _getTargetSpeed * DeltaTime_F;
-
-		// 가속도 운동
-		// 
-		// 안되는 버전
-		// float accSpeed = -(_getTargetSpeed * _accTime) + _getTargetSpeed;
-		// _corLookPos = _prevLookPos + lookDir.Normalize() * accSpeed;
-		// 
-		// 그나마 나은 버젼 (등가속도)
 		_corLookPos = _prevLookPos + lookDir.Normalize() * _getTargetSpeed * DeltaTime_F;
-
-		// 카메라 진동
-		// _corLookPos = _prevLookPos + lookDir.Normalize() * (_getTargetSpeed / _accTime) * DeltaTime_F;
 	}
 
 	// 둘 사이의 차이값을 뺀 것이 _corLookPos가 쫒아가야할 방향이다.
